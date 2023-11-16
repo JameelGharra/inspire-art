f7b62733baefe1b64b2157d4ebcc5b7caa453365
JameelGharra <jameelgharra@gmail.com>
2023-11-16 14:56:19 +0200

    Grounds for the prompt input component - including transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 import Header from '@/components/Header'
+import PromptInput from '@/components/PromptInput'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      {/* Header */}
+
       <Header />
-      {/* Prompt input */}
+      <PromptInput />
       {children}
       </body>
     </html>
---
+++ components/PromptInput.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function PromptInput() {
+  return (
+    <div className='m-10'>
+        <form className='flex flex-col lg:flex-row shadow-md
+        shadow-slate-400/10 border rounded-md lg:divide-x'>
+            <textarea className='flex-1 p-4 outline-none rounded-md' />
+            <button>Generate</button>
+            <button 
+                className='p-4 bg-violet-400 text-white transition-colors 
+                duration-200 font-bold disabled:text-gray-300 
+                disabled:cursor-not-allowed disabled:bg-gray-400'
+            >
+                Use Suggestion
+            </button>
+            <button 
+                className='p-4 bg-white text-violet-500 border-none
+                transition-colors duration-200 rounded-b-md md:rounded-r-md
+                md:rounded-bl-none font-bold'
+            
+            >
+                Suggest more
+            </button>
+        </form>
+    </div>
+  )
+}
