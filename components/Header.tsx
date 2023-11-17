1c65f90b733b9b80a3b245e4e737ebbbbe981a22
JameelGharra <jameelgharra@gmail.com>
2023-11-17 19:02:53 +0200

    Styled the viewed images with hover effects

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <header className='sticky top-0 shadow-lg'>
+    <header className='top-0 shadow-lg'>
         <div className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 p-6 text-center flex items-center justify-center">
                 <h1 className="text-3xl font-semibold text-white flex items-center">
                 <Image src="/header_drop.png" alt="logo" width="45" height="50" className="mr-2" />
--- components/Images.tsx
+++ components/Images.tsx
@@ -21,9 +21,27 @@ function Images() {
     console.log(images?.imageUrls)
     return (
         <div>
-            <div>
-                {images?.imageUrls?.map((image: ImageType) => (
-                    <div key= {image.name}>
+            <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3
+            xl:grid-cols-4 2xl:grid-cols-5 px-0 md:px-10'>
+                {images?.imageUrls?.map((image: ImageType, i:number) => (
+                    <div key= {image.name}
+                        className=
+                        {`relative cursor-help 
+                        ${
+                            i === 0 && "md:col-span-2 md:row-span-2"
+                        }
+                        hover:scale-[103%] transition-transform
+                        duration-200 ease-in-out`
+                    }
+                    >
+                        <div className='absolute flex justify-center items-center
+                        w-full h-full bg-white opacity-0 hover:opacity-80
+                         transition-opacity duration-200 z-10'>
+                            <p className='text-center font-light text-lg p-5'>
+                                {/* this removes the timestamp and file extension */}
+                                {image.name.split("_").shift()?.toString().split(".").shift()}
+                            </p>
+                        </div>
                         <Image 
                             src={image.url}
                             alt={image.name}
